03794f5af1bba5d8ceb72d08ba937c1c6ac76177
coderiaser <coderaiser@cloudcmd.io>
2025-02-12 15:48:13 +0200

    chore: lint

--- .madrun.js
+++ .madrun.js
@@ -1,20 +1,15 @@
 'use strict';
 
-const {
-    run,
-    series,
-    parallel,
-} = require('madrun');
+const {run} = require('madrun');
 
 module.exports = {
     'fix:lint': () => run('lint', '--fix'),
-    "lint": () => 'putout lib test',
-    "test": () => 'tape \'test/**/*.js\'',
-    "watch:test": () => run('watcher', 'npm test'),
-    "watch:coverage": () => run('watcher', 'nyc npm test'),
-    "watch:lint": () => run('watcher', 'redrun lint:eslint*'),
-    "watcher": () => 'nodemon -w test -w lib --exec',
-    "coverage": () => 'nyc npm test',
-    "report": () => 'nyc report --reporter=text-lcov | coveralls'
+    'lint': () => 'putout .',
+    'test': () => `tape 'test/**/*.js'`,
+    'watch:test': () => run('watcher', 'npm test'),
+    'watch:coverage': () => run('watcher', 'nyc npm test'),
+    'watch:lint': () => run('watcher', 'redrun lint:eslint*'),
+    'watcher': () => 'nodemon -w test -w lib --exec',
+    'coverage': () => 'c8 npm test',
+    'report': () => 'c8 report --reporter=text-lcov | coveralls',
 };
-
